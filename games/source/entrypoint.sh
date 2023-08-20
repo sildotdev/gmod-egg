74f0f923a0a60f5810e999f0b25373fcc03109c1
sil <git@sil.dev>
2023-08-19 23:43:27 -0500

    Initial commit

--- games/source/entrypoint.sh
+++ games/source/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright (c) 2021 Matthew Penner
+# Copyright (c) 2023 Matthew Penner & sil.dev
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -58,15 +58,63 @@ if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
     if [ ! -z ${SRCDS_APPID} ]; then
         ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
     else
-        echo -e "No appid set. Starting Server"
+        echo -e "No appid set."
     fi
 
 else
-    echo -e "Not updating game server as auto update was set to 0. Starting Server"
+    echo -e "Not updating game server as auto update was set to 0."
 fi
 
-# Display the command we're running in the output, and then execute it with the env
-# from the container itself.
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
-# shellcheck disable=SC2086
-exec env ${PARSED}
+# Checking and pulling/cloning/updating repositories
+BRANCH=${BRANCH:-master} # Default branch if BRANCH variable is not defined
+
+# Function to clone or update a repository
+clone_or_update_repo() {
+    local repo_url="$1"
+    local target_folder="$HOME/$2"
+    local target_notempty="$3"
+
+    if [ -z "$repo_url" ]; then
+        echo "Repo URL for $target_folder is not defined. Skipping."
+        return
+    fi
+
+    # Using the GITHUB_TOKEN for authentication
+    if [ -n "$GITHUB_TOKEN" ]; then
+        repo_url="https://x-access-token:${GITHUB_TOKEN}@${repo_url#https://}"
+    fi
+
+    if [ "$target_notempty" == "true" ]; then
+        if [ -d "$target_folder" ]; then
+            # Temporarily move existing files
+            mkdir -p "${target_folder}_backup"
+            mv "$target_folder"/* "${target_folder}_backup/"
+            git clone --branch "$BRANCH" "$repo_url" "$target_folder"
+            # Copy files from backup to target only if they don't exist in target
+            cp -n -r "${target_folder}_backup/"* "$target_folder/"
+            rm -r "${target_folder}_backup"
+        else
+            git clone --branch "$BRANCH" "$repo_url" "$target_folder"
+        fi
+    else
+        if [ -d "$target_folder/.git" ]; then
+            echo "Updating repository in $target_folder"
+            git -C "$target_folder" pull origin "$BRANCH"
+        else
+            echo "Cloning repository from $repo_url to $target_folder"
+            git clone --branch "$BRANCH" "$repo_url" "$target_folder"
+        fi
+    fi
+}
+
+# Delete addons folder if it's empty
+if [ -d "$HOME/garrysmod/addons" ] && [ -z "$(ls -A $HOME/garrysmod/addons)" ]; then
+    rm -r "$HOME/garrysmod/addons"
+fi
+
+# Clone or update repositories
+clone_or_update_repo "$REPO_GAMEMODE" "garrysmod/gamemodes/${FOLDER_GAMEMODE}"
+clone_or_update_repo "$REPO_BASE" "garrysmod/gamemodes/${FOLDER_BASE}"
+clone_or_update_repo "$REPO_ADDONS" "garrysmod/addons"
+clone_or_update_repo "$REPO_BIN" "garrysmod/lua/bin"
+clone_or_update_repo "$REPO_CFG" "garrysmod/cfg" "true"
